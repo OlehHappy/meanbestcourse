ccfa9a3c13978a74c321f52088e427bb6da85bfb
OlehHappy <olegtranslator@gmail.com>
2016-05-31 18:42:25 +0200

    login and signup now is working

--- app/routes/api.js
+++ app/routes/api.js
@@ -28,12 +28,17 @@ module.exports = function(app, express) {
                   password: req.body.password
     });
 
+    var token = createToken(user);
     user.save(function(err) {
       if(err) {
         res.send(err);
         return;
       }
-      res.json({message: 'User has been created!'});
+      res.json({
+        success: true,
+        message: 'User has been created!',
+        token: token
+      });
     });
   });
 
@@ -53,7 +58,7 @@ module.exports = function(app, express) {
 
       User.findOne({
         username: req.body.username
-      }).select('password').exec(function(err, user) {
+      }).select('name username password').exec(function(err, user) {
 
         if(err) throw err;
 
